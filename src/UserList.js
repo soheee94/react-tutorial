c7b45ad2e070871aa495fb83006fe1d86c0ea8b4
soheee94 <hsh8616@naver.com>
2019-11-12 17:51:48 +0900

    Chapter1_16

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 function User({user, onRemove, onToggle}){
     const {username, email, id, active} = user;
+    useEffect(() => {
+        // user값이 바뀔 때 마다 실행
+        console.log('user값이 설정됨');
+        console.log(user);
+        return () => {
+            console.log('user값이 바뀌기 전');
+            console.log(user);
+        }
+    }, [user])
+    // useEffect(() => {
+    //     // 마운트
+    //     // props -> state
+    //     // API
+    //     // D3, Video.js
+    //     // setInterval, setTimeout
+    //     // 나타나고 나서라서 DOM에 접근 가능
+    //     console.log('내 눈 앞에 나타나');
+    //     // 언마운트
+    //     return () => {
+    //         // clearInterval, clearTimeout
+    //         // 라이브러리 인스턴스 제거
+    //         // 뒷정리
+    //         console.log('사라져');
+    //     }
+    // }, []);
     return(
         <div>
             <b style={{
