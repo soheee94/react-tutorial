23436bb8f642e8be76dab50ee4c2b353166fedf6
soheee94 <hsh8616@naver.com>
2019-11-12 16:28:34 +0900

    Chapter 1_15

--- src/App.js
+++ src/App.js
@@ -22,17 +22,20 @@ function App() {
     {
         id:1,
         username: 'sohee1',
-        email: 'hsh8616@naver.com'
+        email: 'hsh8616@naver.com',
+        active: true
     },
     {
         id:2,
         username: 'sohee2',
-        email: 'hsh8616@gmail.com'
+        email: 'hsh8616@gmail.com',
+        active: false
     },
     {
         id:3,
         username: 'sohee3',
-        email: 'hsh8616@hanmail.com'
+        email: 'hsh8616@hanmail.com',
+        active: false
     }
   ]);
 
@@ -60,13 +63,23 @@ function App() {
     setUsers(users.filter(user => user.id !== id));
   }
 
+  const onToggle = id => {
+    setUsers(users.map(
+      user => user.id === id
+        ? {...user, active: !user.active} //불변성 유지
+        : user 
+    ))
+  }
+
   return (
     <>
       <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
-      <UserList users={users} onRemove={onRemove}/>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
     </>
     
   );
 }
+
+// 배열 수정(map) 추가(concat) 제거(filter) 
 
 export default App;
