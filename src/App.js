63f6aca821cb7e8ba316fafc7b83a7ae75832b0e
soheee94 <hsh8616@naver.com>
2019-11-14 15:43:20 +0900

    Chapter1_17

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useMemo } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+
+function countActiveUsers(users){
+  console.log('활성 사용자 수를 세는 중');
+  return users.filter(user => user.active).length;
+}
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -71,10 +76,12 @@ function App() {
     ))
   }
 
+  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <>
       <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
       <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <div>활성 사용자 수 : {count}</div>
     </>
     
   );
