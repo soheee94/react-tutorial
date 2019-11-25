fe6f2df200929ff9ee6c765e93b87fda4ed222f7
soheee94 <hsh8616@naver.com>
2019-11-25 18:06:55 +0900

    Chapter1_23

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Counter from './Counter';
+import ContextSample from './ContextSample';
 import * as serviceWorker from './serviceWorker';
 
 // root 인걸 찾아서 그 안에 app을 넣겟다
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ContextSample />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
