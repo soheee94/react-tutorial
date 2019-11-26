cd8c6538ebdb91235a8df954a0f96c6e3ecad23a
soheee94 <hsh8616@naver.com>
2019-11-26 17:12:31 +0900

    [H/W] Chapter1_22

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import ContextSample from './ContextSample';
 import * as serviceWorker from './serviceWorker';
 
 // root 인걸 찾아서 그 안에 app을 넣겟다
-ReactDOM.render(<ContextSample />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/useInputs.js
+++ src/useInputs.js
@@ -1,12 +1,50 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useReducer } from 'react';
 
+// function useInputs(initialForm){
+//     const [ form, setForm ] = useState(initialForm);
+//     const onChange = useCallback(e =>{
+//         const { name, value } = e.target;
+//         setForm(form => ({...form, [name] : value}))
+//     }, []);
+//     const reset = useCallback(() => setForm(initialForm),[]);
+//     return [form, onChange, reset];
+// }
+function reducer(state, action){
+    switch(action.type){
+        case 'CHANGE_INPUT' :
+            return {
+                ...state,
+                [action.name] : action.value
+            }
+        case 'RESET_INPUT' :
+            return {
+                ...action.form
+            }
+        default :
+            throw new Error('Unhandled action');
+    }
+}
 function useInputs(initialForm){
-    const [ form, setForm ] = useState(initialForm);
-    const onChange = useCallback(e =>{
-        const { name, value } = e.target;
-        setForm(form => ({...form, [name] : value}))
-    }, []);
-    const reset = useCallback(() => setForm(initialForm),[]);
+    const [ form, dispatch ] = useReducer(reducer, initialForm);
+    const onChange = useCallback(e => {
+        const {name, value} = e.target;
+        dispatch({
+            type : 'CHANGE_INPUT',
+            name,
+            value
+        })
+    },[]);
+    
+    const reset = useCallback(() => {
+        dispatch({
+            type : 'RESET_INPUT',
+            form : {
+                username:'',
+                email:''
+            }
+        })
+    },[])
+
     return [form, onChange, reset];
 }
 
