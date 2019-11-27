4b1b8761f0aa1153964f3f16a9c6f9d3c50e484d
soheee94 <hsh8616@naver.com>
2019-11-27 17:12:25 +0900

    Chapter1_25~26

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useRef, useReducer, useMemo, useCallback, createContext } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+import produce from 'immer';
 // import useInputs from './useInputs';
+
+window.produce = produce;
 
 function countActiveUsers(users){
   console.log('활성 사용자 수를 세는 중');
@@ -34,24 +37,39 @@ const initialState = {
 function reducer(state, action){
   switch(action.type){
     case 'CREATE_USER':
-      return{
-        inputs : initialState.inputs,
-        users: state.users.concat(action.user)
-      }
+      return produce(state, draft => {
+        draft.users.push(action.user)
+      })
+      // 이런 형태는 굳이 사용하지 않아도 되고
+      // return{
+      //   // inputs : initialState.inputs,
+      //   users: state.users.concat(action.user)
+      // }
     case 'TOGGLE_USER':
-      return{
-        ...state,
-        users: state.users.map(user =>
-          user.id === action.id
-          ? {...user, active: !user.active}
-          : user
-        )
-      }
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      })
+      // 이렇게 코드 구조가 복잡할 경우는 IMMER 를 사용하는 것이 좋다
+      // 훨씬 깔끔해지기 때문에!
+      // return{
+      //   ...state,
+      //   users: state.users.map(user =>
+      //     user.id === action.id
+      //     ? {...user, active: !user.active}
+      //     : user
+      //   )
+      // }
     case 'REMOVE_USER':
-      return{
-        ...state,
-        users : state.users.filter(user=> user.id !== action.id)
-      }
+      // 이런 형태도 굳이 사용하지 않아도 된다!
+      // return{
+      //   ...state,
+      //   users : state.users.filter(user=> user.id !== action.id)
+      // }
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      })
     default :
       throw new Error('Unhandled action');
   }
