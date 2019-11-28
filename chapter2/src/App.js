c8916d94eaed5107a768ebf65a6fda30bcb0b33b
한소희 <hsh8616@naver.com>
2019-11-28 22:33:29 +0900

    Chapter2_1

---
+++ chapter2/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+import Button from './component/Button';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="buttons">
+        <Button>BUTTON</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
