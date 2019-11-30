537aa990bcf249df01d9edb6f0bd2e3ebdb8cd84
한소희 <hsh8616@naver.com>
2019-11-30 19:05:53 +0900

    Chapter2_6

--- chapter2/src/App.js
+++ chapter2/src/App.js
@@ -1,73 +1,18 @@
-import React from 'react';
-import './App.scss';
-import Button from './component/Button';
-
-import './App.css';
+import React, { useState } from 'react';
+import CheckBox from './component/CheckBox';
 
 function App() {
+  const [check, setCheck] = useState(false);
+  const onChange = e => {
+    setCheck(e.target.checked);
+  };
   return (
-    <div className="App">
-      <div className="buttons">
-        <Button size="large" color="gray">
-          BUTTON
-        </Button>
-        <Button>BUTTON</Button>
-        <Button size="small" color="pink">
-          BUTTON
-        </Button>
-      </div>
-
-      <div className="buttons">
-        <Button size="large" color="gray">
-          BUTTON
-        </Button>
-        <Button>BUTTON</Button>
-        <Button size="small" color="pink">
-          BUTTON
-        </Button>
-      </div>
-
-      <div className="buttons">
-        <Button size="large" color="gray">
-          BUTTON
-        </Button>
-        <Button>BUTTON</Button>
-        <Button size="small" color="pink">
-          BUTTON
-        </Button>
-      </div>
-
-      <div className="buttons">
-        <Button size="large" color="gray" outline>
-          BUTTON
-        </Button>
-        <Button outline>BUTTON</Button>
-        <Button size="small" color="pink">
-          BUTTON
-        </Button>
-      </div>
-
-      <div className="buttons">
-        <Button size="large" color="gray" outline fullWidth>
-          BUTTON
-        </Button>
-        <Button outline fullWidth>
-          BUTTON
-        </Button>
-        <Button
-          size="small"
-          color="pink"
-          fullWidth
-          onClick={() => {
-            console.log('클릭!');
-          }}
-          onMouseMove={() => {
-            console.log('마우스무브!');
-          }}
-        >
-          BUTTON
-        </Button>
-      </div>
+    <div>
+      <CheckBox onChange={onChange} checked={check}></CheckBox>
+      <p>
+        <b>check : </b>
+        {check ? 'true' : 'false'}
+      </p>
     </div>
   );
 }
