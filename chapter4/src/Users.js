347a0a9f141a8803de7903851e26595cbd354bd0
soheee94 <hsh8616@naver.com>
2019-12-05 18:07:47 +0900

    Chapter4_2

--- chapter4/src/Users.js
+++ chapter4/src/Users.js
@@ -1,33 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
-function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+function reducer(state, action) {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case 'SUCCESS':
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case 'ERROR':
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`unhandled action type : ${action.type}`);
+  }
+}
 
+function Users() {
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
   const fetchUsers = async () => {
     try {
-      // 1. 초기화
-      setError(null);
-      setUsers(null);
-      // 2. 로딩 시작
-      setLoading(true);
+      // 1. 초기화 / 로딩 시작
+      dispatch({ type: 'LOADING' });
       const response = await axios.get(
         'https://jsonplaceholder.typicode.com/users',
       );
-      // 3. 사용자 데이터 설정
-      setUsers(response.data);
+      // 2. 사용자 데이터 설정
+      dispatch({ type: 'SUCCESS', data: response.data });
     } catch (e) {
-      setError(e);
+      dispatch({ type: 'ERROR', error: e });
     }
-    setLoading(false);
+    // setLoading(false);
   };
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
+  const { loading, data: users, error } = state;
   if (loading) return <div>로딩중!</div>;
   if (error) return <div>에러 발생!</div>;
   if (!users) return null;
