2f59572ee7d742f37f08a02c12f8b818d61f3885
한소희 <hsh8616@naver.com>
2019-12-15 21:53:37 +0900

    Chapter5_4

--- chapter5/src/App.js
+++ chapter5/src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Profiles from './Profiles';
+import HistorySample from './HistorySample';
 
 function App() {
   return (
@@ -14,11 +15,15 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/history">예제</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
       <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 }
---
+++ chapter5/src/HistorySample.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+
+function HistorySample({ history }) {
+  const goBack = () => {
+    history.goBack();
+  };
+  const goHome = () => {
+    history.push('/');
+  };
+
+  useEffect(() => {
+    console.log(history);
+    const unblock = history.block('떠날고야?');
+    return () => {
+      unblock();
+    };
+  }, [history]);
+  return (
+    <div>
+      <button onClick={goBack}>뒤로가기</button>
+      <button onClick={goHome}>홈으로</button>
+    </div>
+  );
+}
+
+export default HistorySample;
