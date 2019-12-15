891c7855996432372cec4669ddef6c608d41b213
한소희 <hsh8616@naver.com>
2019-12-15 21:19:32 +0900

    Chapter5_3

--- chapter5/src/App.js
+++ chapter5/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
       <hr />
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 }
