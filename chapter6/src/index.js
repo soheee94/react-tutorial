ce52d26f27a821efacef4319b9821d9e982da2ac
한소희 <hsh8616@naver.com>
2019-12-17 21:25:46 +0900

    Chapter6_6

--- chapter6/src/index.js
+++ chapter6/src/index.js
@@ -7,9 +7,11 @@ import { createStore } from 'redux';
 // ./modules/index.js = ./modules
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 // 스토어 생성
-const store = createStore(rootReducer);
+// 리덕스 개발자 도구 활성화
+const store = createStore(rootReducer, composeWithDevTools());
 // 스토어 상태 확인
 console.log(store.getState());
 
