f89eb8998e1a92b48e2e2488994d8f6e0d34576b
한소희 <hsh8616@naver.com>
2019-12-21 17:10:21 +0900

    Chapter 6_7~9

--- chapter6/src/App.js
+++ chapter6/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import CounterContainer from './containers/CounterContainer';
+import TodoContainer from './containers/TodoContainer';
 
 function App() {
   return (
     <div>
       <CounterContainer />
+      <hr />
+      <TodoContainer />
     </div>
   );
 }
---
+++ chapter6/src/components/Todos.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+// 컴포넌트 최적화를 위해 React.memo를 사용
+const TodoItem = React.memo(function TodoItem({ todo, onToggle }) {
+  return (
+    <li
+      style={{ textDecoration: todo.done ? 'line-through' : 'none' }}
+      onClick={() => onToggle(todo.id)}
+    >
+      {todo.text}
+    </li>
+  );
+});
+
+const TodoList = React.memo(function TodoList({ todos, onToggle }) {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <TodoItem key={todo.id} todo={todo} onToggle={onToggle} />
+      ))}
+    </ul>
+  );
+});
+
+function Todos({ todos, onToggle, onCreate }) {
+  // 리덕스를 사용한다고 해서 모든 상태를 리덕스에서 관리해야하는 것은 아닙니다.
+  const [text, setText] = useState('');
+  const onChange = e => setText(e.target.value);
+  const onSubmit = e => {
+    e.preventDefault();
+    onCreate(text);
+    setText('');
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={text}
+          onChange={onChange}
+          placeholder="할 일 입력 하세요!"
+        />
+        <buttom type="submit">등록</buttom>
+      </form>
+      <TodoList todos={todos} onToggle={onToggle} />
+    </div>
+  );
+}
+
+export default React.memo(Todos);
