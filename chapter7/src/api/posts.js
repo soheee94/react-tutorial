cc08f1d0592414981046078ba29f34bde07af6d7
한소희 <hsh8616@naver.com>
2020-03-24 00:07:46 +0900

    Chpater7_5~9

---
+++ chapter7/src/api/posts.js
@@ -0,0 +1,30 @@
+// n 밀리세컨동안 기다리는 프로미스 함수
+const sleep = n => new Promise(resolve => setTimeout(resolve, n));
+
+const posts = [
+  {
+    id: 1,
+    title: '테스트',
+    body: '이거는 테스트 1번이에요',
+  },
+  {
+    id: 2,
+    title: '테스트2',
+    body: '이거는 테스트 2번이에요',
+  },
+  {
+    id: 3,
+    title: '테스트',
+    body: '이거는 테스트 3번이에요',
+  },
+];
+
+export const getPosts = async () => {
+  await sleep(500);
+  return posts;
+};
+
+export const getPostById = async id => {
+  await sleep(500);
+  return posts.find(post => post.id === id);
+};
---
+++ chapter7/src/lib/asyncUtils.js
@@ -0,0 +1,74 @@
+// Promise에 기반한 Thunk를 만들어주는 함수입니다.
+export const createPromiseThunk = (type, promiseCreator) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+  // 이 함수는 promiseCreator가 단 하나의 파라미터만 받는다는 전제하에 작성되었습니다.
+  // 만약 여러 종류의 파라미터를 전달해야하는 상황에서는 객체 타입의 파라미터를 받아오도록 하면 됩니다.
+  // 예: writeComment({ postId: 1, text: '댓글 내용' });
+  // (dispatch, getState)
+  return param => async dispatch => {
+    // 요청 시작
+    dispatch({ type, param });
+    try {
+      // 결과물의 이름은 payload로 통일
+      const payload = await promiseCreator(param);
+      // 성공
+      dispatch({ type: SUCCESS, payload });
+    } catch (e) {
+      // 실패
+      dispatch({ type: ERROR, payload: e, error: true });
+    }
+  };
+};
+
+// 리듀서에서 사용할 수 있는 유틸 함수
+export const reducerUtils = {
+  // 초기상태
+  initial: (data = null) => ({
+    data,
+    loading: false,
+    error: null,
+  }),
+  // 로딩중 상태
+  loading: (prevState = null) => ({
+    loading: true,
+    data: prevState,
+    error: null,
+  }),
+  // 성공 상태
+  success: payload => ({
+    loading: false,
+    data: payload,
+    error: null,
+  }),
+  // 실패 상태
+  error: error => ({
+    error,
+    loading: false,
+    data: null,
+  }),
+};
+
+export const handleAsyncActions = (type, key) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+  return (state, action) => {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: reducerUtils.loading(),
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: reducerUtils.success(action.payload),
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: reducerUtils.error(action.payload),
+        };
+      default:
+        return state;
+    }
+  };
+};
