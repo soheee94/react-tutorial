7977c32c9311ebd0f2b2ac411148b8cfa8ad7ee6
soheee94 <hsh8616@naver.com>
2020-03-24 11:39:37 +0900

    Chapter7_12~13

--- chapter7/src/containers/PostContainer.js
+++ chapter7/src/containers/PostContainer.js
@@ -2,19 +2,22 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getPost, clearPost } from '../modules/posts';
 import Post from '../components/Post';
+import { reducerUtils } from '../lib/asyncUtils';
 
 function PostContainer({ postId }) {
-  const { data, loading, error } = useSelector(state => state.posts.post);
+  const { data, loading, error } = useSelector(
+    state => state.posts.post[postId] || reducerUtils.initial(),
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getPost(postId));
-    return () => {
-      dispatch(clearPost());
-    };
+    // return () => {
+    //   dispatch(clearPost());
+    // };
   }, [postId, dispatch]);
 
-  if (loading) return <div>로딩중...</div>;
+  if (loading && !data) return <div>로딩중...</div>;
   if (error) return <div>에러 발생!</div>;
   if (!data) return null;
 
--- chapter7/src/modules/posts.js
+++ chapter7/src/modules/posts.js
@@ -3,6 +3,8 @@ import {
   reducerUtils,
   createPromiseThunk,
   handleAsyncActions,
+  createPromiseThunkById,
+  handleAsyncActionsById,
 } from '../lib/asyncUtils';
 
 /* 액션 타입 */
@@ -18,20 +20,20 @@ const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
 // 포스트 비우기
-const CLEAR_POST = 'CLEAR_POST';
+// const CLEAR_POST = 'CLEAR_POST';
 
 // thunk 를 사용 할 때, 꼭 모든 액션들에 대하여 액션 생성함수를 만들 필요는 없습니다.
 // 그냥 thunk 함수에서 바로 액션 객체를 만들어주어도 괜찮습니다.
 
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
 // thunk 함수에서도 파라미터를 받아와서 사용 할 수 있습니다.
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
 
-export const clearPost = () => ({ type: CLEAR_POST });
+// export const clearPost = () => ({ type: CLEAR_POST });
 
 const initialState = {
   posts: reducerUtils.initial(),
-  post: reducerUtils.initial(),
+  post: {},
 };
 
 export default function posts(state = initialState, action) {
@@ -43,12 +45,12 @@ export default function posts(state = initialState, action) {
     case GET_POST:
     case GET_POST_SUCCESS:
     case GET_POST_ERROR:
-      return handleAsyncActions(GET_POST, 'post')(state, action);
-    case CLEAR_POST:
-      return {
-        ...state,
-        post: reducerUtils.initial(),
-      };
+      return handleAsyncActionsById(GET_POST, 'post', true)(state, action);
+    // case CLEAR_POST:
+    //   return {
+    //     ...state,
+    //     post: reducerUtils.initial(),
+    //   };
     default:
       return state;
   }
