76afeb565be01881c0af38558bc093b1ca133915
soheee94 <hsh8616@naver.com>
2020-03-24 12:28:46 +0900

    Chapter7_15

--- chapter7/src/api/posts.js
+++ chapter7/src/api/posts.js
@@ -1,30 +1,42 @@
 // n 밀리세컨동안 기다리는 프로미스 함수
-const sleep = n => new Promise(resolve => setTimeout(resolve, n));
+// const sleep = n => new Promise(resolve => setTimeout(resolve, n));
 
-const posts = [
-  {
-    id: 1,
-    title: '테스트',
-    body: '이거는 테스트 1번이에요',
-  },
-  {
-    id: 2,
-    title: '테스트2',
-    body: '이거는 테스트 2번이에요',
-  },
-  {
-    id: 3,
-    title: '테스트',
-    body: '이거는 테스트 3번이에요',
-  },
-];
+// const posts = [
+//   {
+//     id: 1,
+//     title: '테스트',
+//     body: '이거는 테스트 1번이에요',
+//   },
+//   {
+//     id: 2,
+//     title: '테스트2',
+//     body: '이거는 테스트 2번이에요',
+//   },
+//   {
+//     id: 3,
+//     title: '테스트',
+//     body: '이거는 테스트 3번이에요',
+//   },
+// ];
+
+// export const getPosts = async () => {
+//   await sleep(500);
+//   return posts;
+// };
+
+// export const getPostById = async id => {
+//   await sleep(500);
+//   return posts.find(post => post.id === id);
+// };
+
+import axios from 'axios';
 
 export const getPosts = async () => {
-  await sleep(500);
-  return posts;
+  const response = await axios.get('http://localhost:4000/posts');
+  return response.data;
 };
 
 export const getPostById = async id => {
-  await sleep(500);
-  return posts.find(post => post.id === id);
+  const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  return response.data;
 };
